33b8ec9b9d4c97d112eca12c615523b9687f6300
AcriCAA <acri.corey@gmail.com>
2015-08-06 10:41:40 -0400

    changing js to grab geoJSON Some more

--- geoJSONConvert.js
+++ geoJSONConvert.js
@@ -1,7 +1,30 @@
-var geoData = JSON.parse('https://api.phila.gov/bike-share-stations/v1');
-var features = geoData.features;
+// ----receive function----v
+function get_json(url, callback) {
+    http.get(url, function(res) {
+        var body = '';
+        res.on('data', function(chunk) {
+            body += chunk;
+        });
 
-var feature = geoData.features[0];
+        res.on('end', function() {
+            var response = JSON.parse(body);
+// call function ----v
+            callback(response);
+        });
+    });
+}
+
+         // -----------the url---v         ------------the callback---v
+var mydata = get_json("https://api.phila.gov/bike-share-stations/v1", function (resp) {
+    console.log(resp);
+});
+
+
+
+//var geoData = JSON.parse('https://api.phila.gov/bike-share-stations/v1');
+//var features = geoData.features;
+
+//var feature = geoData.features[0];
 
 
 console.log(geoData);
